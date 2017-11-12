8633600164e3936484b698bf2b70cdc9f730f252
Sadra Teymourian <Sadra.Teymourian@outlook.com>
2017-11-12 11:35:52 +0330

    Add Paper API

--- routes/papers.js
+++ routes/papers.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var Paper = require("../schemas/paper");
 
 router.post('/add', function (req, res) {
-    
+    var newPaper = new Paper({
+        title : req.body.title,
+        text : req.body.text
+    });
+    newPaper.save();
 });
 
 module.exports = router;
