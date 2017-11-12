38e178695758648065e64f6e6a33662d1f530ba8
Sadra Teymourian <Sadra.Teymourian@outlook.com>
2017-11-12 13:49:04 +0330

    User Registration

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,33 @@
 var express = require('express');
+var User = require("../schemas/user");
+var passport = require("passport");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post("/register", function (req, res) {
+  User.register(new User({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    signature: req.body.signature,
+    email: req.body.email,
+    phone: req.body.phone,
+  }), req.body.password, function (err, registeredItem) {
+    if (err) {
+      res.send({
+        registeredStatus: false,
+        error: err
+      });
+    }
+    else {
+      res.send({
+        registeredStatus: true,
+        registeredUser: registeredItem
+      });
+    }
+  })
+})
 module.exports = router;
