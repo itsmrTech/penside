58e7bdd61802ea0d97abaefe3bcf9033cc851d2e
mrTech <shamot.group@gmail.com>
2017-11-12 11:19:21 +0330

    papers router added

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var papersRouter = require('./routes/papers');
+
 
 var app = express();
 
@@ -27,6 +29,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/papers', papersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
