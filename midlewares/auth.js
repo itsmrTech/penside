96c03939dc5f10daee0b16cdfa920958dcc6e2f9
Sadra Teymourian <Sadra.Teymourian@outlook.com>
2017-11-23 15:53:08 +0330

    Adding Paper

--- midlewares/auth.js
+++ midlewares/auth.js
@@ -1,36 +1,33 @@
-var express = require('express');
-var Device = require("../schemas/device");
-var User = require("../schemas/user")
+var User = require("../schemas/user");
+var Device = require("../schemas/device")
 var jwt = require('jsonwebtoken');
 
-function checkToken(req, res) {
-    jwt
-        .verify(req.body.token, "3Dozde Boz Dozd Raftan Boz Dozdi YeDozde Boz Dozd 3Boz Dozdid 3 Dozde Boz Dozd B" +
-        "e Ye Dozde Boz Dozd Goftan Ma Ke 3 Dozde Boz Dozdim Ye Boz Dozdidim Toke YE Dozd" +
-        "e Boz Dozdi 3 Boz Dozdidi.",
-        function (err, decoded) {
-            if (err) {
-                res.send({ auth: false, error: err });
-            } else {
-                Device
-                    .findOne({
-                        _id: decoded.id
-                    }, function (err, foundDevice) {
-                        if (err) {
-                            res.send({ foundDevice: false, error: err });
-                        } else {
-                            User
-                                .findOne({ _id: foundDevice.user })
-                                .exec(function (err, foundUser) {
-                                    if (err) {
-                                        return res.send({ foundUser: false, auth: false, error: err })
-                                    }
-
-                                })
-                        }
-                    })
-            }
+var exp = {}
+exp.checkToken = function (req, res, next) {
+    //Verifing and decoding token which has device id
+    jwt.verify(req.body.token, "3Dozde Boz Dozd Raftan Boz Dozdi YeDozde Boz Dozd 3Boz Dozdid 3 Dozde Boz Dozd Be Ye Dozde Boz Dozd Goftan Ma Ke 3 Dozde Boz Dozdim Ye Boz Dozdidim Toke YE Dozde Boz Dozdi 3 Boz Dozdidi."
+        , function (err, decoded) {
+            if (err)
+                return res.status(401).json({ error: err, auth: false })
+            // Find device from decoded token
+            Device.findOne({ _id: decoded.id }, function (err, foundDevice) {
+                if (err)
+                    return res.status(500).json({ error: err, auth: false, foundDevice: false })
+                if (!foundDevice)
+                    return res.status(404).json({ error: "Device Not Found", auth: false, foundDevice: false })
+                //Find user from found device
+                User.findOne({ _id: foundDevice.user }, function (err, foundUser) {
+                    if (err)
+                        return res.status(500).json({ error: err, auth: false, foundDevice: true, foundUser: false })
+                    if (!foundUser) {
+                        return res.status(404).json({ error: "User Not Found", auth: false, foundDevice: true, foundUser: false })
+                    }
+                    // overwriting json for returning user object
+                    req.user = foundUser;
+                    next();
+                })
+            })
         })
+}
 
-};
-module.exports ={checkToken : checkToken} ;
+module.exports = exp;
